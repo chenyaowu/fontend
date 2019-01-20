e6293e25999b07795103eca9c44971380afd0be2
chenyaowu <chenyaowu15@gmail.com>
2019-01-20 12:17:15 +0800

    js函数
    js函数

---
+++ JS/js函数.js
@@ -0,0 +1,29 @@
+/*
+函数声明
+function fun() {
+	
+}
+
+函数表达式
+var fun = function() {
+	
+}
+
+(function() {
+	
+})();
+
+return function() {
+	
+}
+
+var fun1 = function fun(){
+	
+}
+
+Function构造器
+var func = new Function('a','b','console.log(a+b);');
+
+var func = Function('a','b','console.log(a+b);');
+
+*/
